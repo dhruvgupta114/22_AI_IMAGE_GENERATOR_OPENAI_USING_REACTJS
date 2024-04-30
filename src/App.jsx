94edff96465a7b1ef01bf66ff2b22d36292cf322
Dhruv Gupta <125383912+dhruvgupta114@users.noreply.github.com>
2024-04-30 22:15:57 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import ImageGenerator from './Components/imageGenerator/ImageGenerator'
+
+
+function App() {
+
+  return (
+    <>
+      <ImageGenerator/>
+    </>
+  )
+}
+
+export default App
